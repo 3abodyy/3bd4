11db8a0a02933796cbfc4ae8a6046eb863a01aee
3abodyy <47701080+3abodyy@users.noreply.github.com>
2019-02-16 23:31:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("546431295786582038")
 setInterval(function() {
-channel.send(`MR.3aBooDy 95 ~ Welcome To The Hell`);
+channel.send( Mr.3aBooDyy ~ Welcome To Hell ^_^ );
 }, 30)
 })
 
